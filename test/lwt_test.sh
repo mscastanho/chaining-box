58a28d4387af2d22d986a4e62c39e60b718ab936
Matheus Castanho <mscastanho@gmail.com>
2018-10-31 18:22:17 -0300

    Updated encap test script

--- test/lwt_test.sh
+++ test/lwt_test.sh
@@ -9,6 +9,11 @@ IPVETH1="10.1.0.51"
 NSCMD0="ip netns exec $NS0"
 NSCMD1="ip netns exec $NS1"
 DUMPFILE=tcpdump_tmp.out
+KEEPINFRA=false
+
+if [ "$1" == "keep" ]; then
+    KEEPINFRA=true
+fi
 
 function destroy_infra {
     ip link del $VETH0 2> /dev/null
@@ -18,30 +23,37 @@ function destroy_infra {
     rm -f $DUMPFILE 2> /dev/null
 }
 
+function create_infra {
+    ip netns add $NS0
+    ip netns add $NS1
+
+    ip link add $VETH0 type veth peer name $VETH1
+
+    ip link set $VETH0 netns $NS0
+    ip link set $VETH1 netns $NS1
+
+    $NSCMD0 ip link set dev $VETH0 up
+    $NSCMD1 ip link set dev $VETH1 up
+
+    $NSCMD0 ip addr add $IPVETH0/24 dev $VETH0
+    $NSCMD1 ip addr add $IPVETH1/24 dev $VETH1
+
+    $NSCMD1 ip link set promisc on $VETH1
+
+    # Headroom = sizeof(Ethernet) + sizeof(NSH) = 14 + 8 = 22
+    $NSCMD0 ip route add 0.0.0.0/0 encap bpf headroom 22 \
+        xmit obj ../src/sfc_proxy_kern.o section encap dev $VETH0
+
+    $NSCMD0 ip route del "10.1.0.0"/24 dev $VETH0
+
+    bpftool map update pinned /sys/fs/bpf/tc/globals/nsh_data key hex 0a 01 00 01 0a 01 00 02 03 e8 07 d0 11 value hex 00 02 02 03 00 00 01 ff any
+}
+
 destroy_infra
 echo > /sys/kernel/debug/tracing/trace # Clear trace buffer
 
-ip netns add $NS0
-ip netns add $NS1
+create_infra
 
-ip link add $VETH0 type veth peer name $VETH1
-
-ip link set $VETH0 netns $NS0
-ip link set $VETH1 netns $NS1
-
-$NSCMD0 ip link set dev $VETH0 up
-$NSCMD1 ip link set dev $VETH1 up
-
-$NSCMD0 ip addr add $IPVETH0/24 dev $VETH0
-$NSCMD1 ip addr add $IPVETH1/24 dev $VETH1
-
-$NSCMD1 ip link set promisc on $VETH1
-
-# Headroom = sizeof(Ethernet) + sizeof(NSH) = 14 + 8 = 22
-$NSCMD0 ip route add 0.0.0.0/0 encap bpf headroom 22 \
-    xmit obj ../src/sfc_proxy_kern.o section encap dev $VETH0
-
-$NSCMD0 ip route del "10.1.0.0"/24 dev $VETH0
 
 # $NSCMD0 ip route add "10.1.0.0"/24 encap bpf headroom 14 \
 #    xmit obj ../src/lwt_test_kern.o section push_ll dev $VETH0
@@ -50,7 +62,9 @@ $NSCMD0 ip route del "10.1.0.0"/24 dev $VETH0
 
 $NSCMD1 tcpdump -i $VETH1 -XX not ip6 & > $DUMPFILE
 
-$NSCMD0 ping -c 1 $IPVETH1
+# $NSCMD0 ping -c 1 $IPVETH1
+
+$NSCMD0 python send_packets.py $VETH0 no-nsh
 
 output="$(cat /sys/kernel/debug/tracing/trace)"
 
@@ -67,4 +81,8 @@ echo "================================================="
 echo
 echo "$(cat $DUMPFILE)"
 
-destroy_infra
+if [ KEEPINFRA == "false" ]; then
+    destroy_infra
+else
+    echo "Keeping infra alive."
+fi
