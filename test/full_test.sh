c8961fd46585b92f5f1363c449ac68927cbbefd5
Matheus Castanho <mscastanho@gmail.com>
2018-11-02 18:21:28 -0300

    Added more updates to testing scripts

--- test/full_test.sh
+++ test/full_test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
 function replace_address {
     SUBNET=$1
     HOSTID=$2
@@ -25,7 +30,11 @@ function destroy_infra {
     ip netns del $NS1
     ip link del $VETH0 2> /dev/null
     ip link del $VETH1 2> /dev/null
-    rm -f $DUMPFILE 2> /dev/null
+    # rm -f $DUMPFILE 2> /dev/null
+}
+
+function get_mac {
+    echo "$(ip link show dev $1 | awk '{getline; print}' | awk '{print $2}')"
 }
 
 function create_infra {
@@ -33,13 +42,20 @@ function create_infra {
     
     ip link add $VETH0 type veth peer name $VETH1
 
-    MACVETH1=`ip link | grep -A 1 $VETH1 | awk '{getline; print}' | awk '{print $2}'`
-    MACVETH0=`ip link | grep -A 1 $VETH0 | awk '{getline; print}' | awk '{print $2}'`
+    MACVETH0=$(get_mac $VETH0)
+    MACVETH1=$(get_mac $VETH1)
+
+    # echo "MACVETH0 = $MACVETH0" 
+    # echo "MACVETH1 = $MACVETH1" 
 
     ip link set $VETH1 netns $NS1
     
     ip link set dev $VETH0 up
+    ip link set dev $VETH0 promisc on
+
     $NSCMD1 ip link set dev $VETH1 up
+    $NSCMD1 ip link set lo up
+
 
     ip addr add $IPVETH0/24 dev $VETH0
     $NSCMD1 ip addr add $IPVETH1/24 dev $VETH1
@@ -48,13 +64,13 @@ function create_infra {
     $NSCMD1 arp -s "$IPVETH0" "$MACVETH0"
     
     # Load decap XDP code
-    sudo ip -force link set dev $VETH0 xdp \
+    ip -force link set dev $VETH0 xdp \
         obj ../src/sfc_stages_kern.o sec decap
     
     # Load encap code
     # Headroom = sizeof(Ethernet) + sizeof(NSH) = 14 + 8 = 22
-    ip route add $IPVETH1/32 encap bpf headroom 22 xmit \
-        obj $BPFOBJ section encap dev $VETH0
+    ip route add $IPVETH1/32 encap bpf headroom 22 \
+        xmit obj $BPFOBJ section encap dev $VETH0
 
     ip route del $TESTSUBNET/24 dev $VETH0
     # $NSCMD1 ip route del $TESTSUBNET/24 dev $VETH1
@@ -84,13 +100,15 @@ function create_infra {
 
 destroy_infra
 echo > /sys/kernel/debug/tracing/trace # Clear trace buffer
+pkill tcpdump
 
 create_infra
 
-# $NSCMD1 tcpdump -i $VETH1 -XX not ip6 & > $DUMPFILE
+# tcpdump -i $VETH0 -XX not ip6 & > $DUMPFILE
 
-# hping3 -c 1 -I $VETH0 -a $IPVETH0 -s 1000 -p 2000 --keep -d 20 $IPVETH1 
-# # python send_packets.py $VETH0 no-nsh
+# # $NSCMD1 hping3 -c 1 -I $VETH1 -a $IPVETH1 -s 1000 -p 2000 --keep -d 20 $IPVETH0
+# # echo "MACVETH0 = $MACVETH0" 
+# $NSCMD1 python send_packets.py -i $VETH1 -d $MACVETH0
 
 # output="$(cat /sys/kernel/debug/tracing/trace)"
 
@@ -102,13 +120,13 @@ create_infra
 # echo
 
 # echo "================================================="
-# echo "Output from \"tcpdump -i $VETH1\" -XX"
+# echo "Output from \"tcpdump -i $VETH0\" -XX"
 # echo "================================================="
 # echo
 # echo "$(cat $DUMPFILE)"
 
-# if [ $KEEPINFRA == "false" ]; then
-#     destroy_infra
-# else
-#     echo "Keeping infra alive."
-# fi
+if [ $KEEPINFRA == "false" ]; then
+    destroy_infra
+else
+    echo "Keeping infra alive."
+fi
--- test/send_packets.py
+++ test/send_packets.py
@@ -4,51 +4,66 @@ from scapy.all import *
 from scapy.contrib.nsh import NSH
 import netifaces
 import sys
+import argparse
 
-if len(sys.argv) <= 2:
-    with_nsh = True
-elif sys.argv[2] == "no-nsh":
-    with_nsh = False
-else:
-    print "Usage: ./%s iface [no-nsh]" % sys.argv[0]
-    exit(1)
+# def print_usage():
+    # print '%s -i <interface> [-m <out dest MAC>] [-n] ' % sys.argv[0]
 
-out_iface = sys.argv[1]
-out_src_mac = netifaces.ifaddresses(out_iface)[netifaces.AF_LINK][0]['addr']
-
-html  = """
-<html>
-<head>
-    <title>Messages from the SFs</title>
-</head>
-<body>
-</body>
-</html>
-"""
-
-http_resp  = "HTTP/1.1 200 OK\r\n"
-http_resp += "Server: exampleServer\r\n"
-http_resp += "Content-Length: " + str(len(html)) + "\r\n"
-http_resp += "\r\n"
-http_resp += html
-
-# Generate input packets
-i_eth = Ether(src="AA:BB:CC:DD:EE:FF",dst="AA:AA:AA:AA:AA:AA")
-i_ip = IP(src="10.1.0.50",dst="10.1.0.51")
-i_tcp = TCP(sport=1000,dport=2000)
-
-o_eth = Ether(src=out_src_mac,dst="08:00:27:aa:0b:44")
-
-if with_nsh:
-    in_pkt = o_eth/NSH(NSP=0x1,NSI=0xFF,MDType=0x2)/i_eth/i_ip/i_tcp/http_resp
-else:
-    in_pkt = o_eth/i_ip/i_tcp/http_resp
+def main(argv):
+    parser = argparse.ArgumentParser(prog=sys.argv[0],description='Generates packets with or without NSH')
     
-print "Beautiful packet!\n"
-in_pkt.show()
-print "Ugly packet!\n"
-hexdump(in_pkt)
+    parser.add_argument('-i', '--iface', metavar='<out iface>', 
+        help='Interface to send packet', required=True)
 
-# print out_iface
-sendp(in_pkt,iface=out_iface)
-# wrpcap('nsh.pcap',in_pkt)
+    parser.add_argument('-n', '--no-nsh', action='store_const', const=True,
+        default=False,help='Send packet without NSH')
+
+    parser.add_argument('-d', '--dmac', metavar='<dest MAC>', 
+        default='ab:bc:cd:de:ef:fa', help='Destination MAC address')
+
+    args = vars(parser.parse_args())
+
+    with_nsh = not args['no_nsh'] 
+    out_iface = args['iface']
+    out_dest_mac = args['dmac']  
+    out_src_mac = netifaces.ifaddresses(out_iface)[netifaces.AF_LINK][0]['addr']
+
+    html  = """
+    <html>
+    <head>
+        <title>Messages from the SFs</title>
+    </head>
+    <body>
+    </body>
+    </html>
+    """
+
+    http_resp  = "HTTP/1.1 200 OK\r\n"
+    http_resp += "Server: exampleServer\r\n"
+    http_resp += "Content-Length: " + str(len(html)) + "\r\n"
+    http_resp += "\r\n"
+    http_resp += html
+
+    # Generate input packets
+    i_eth = Ether(src="AA:BB:CC:DD:EE:FF",dst=out_dest_mac)
+    i_ip = IP(src="10.1.0.50",dst="10.1.0.51")
+    i_tcp = TCP(sport=1000,dport=2000)
+
+    o_eth = Ether(src=out_src_mac,dst=out_dest_mac)
+
+    if with_nsh:
+        in_pkt = o_eth/NSH(NSP=0x1,NSI=0xFF,MDType=0x2)/o_eth/i_ip/i_tcp/http_resp
+    else:
+        in_pkt = o_eth/i_ip/i_tcp/http_resp
+        
+    print "Beautiful packet!\n"
+    in_pkt.show()
+    print "Ugly packet!\n"
+    hexdump(in_pkt)
+
+    # print out_iface
+    sendp(in_pkt,iface=out_iface)
+    # wrpcap('nsh.pcap',in_pkt)
+
+if __name__ == "__main__":
+   main(sys.argv)
