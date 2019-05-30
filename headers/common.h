58ee47b7f4a3a86c0d5e3ce32db6da1153769bb4
Matheus Castanho <mscastanho@gmail.com>
2019-05-30 19:30:59 -0300

    Add header from iproute2 with alt map struct

--- headers/common.h
+++ headers/common.h
@@ -6,14 +6,6 @@
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
 
-// Pins a map to the global namespace.
-// This will make it available at
-//      /sys/fs/bpf/tc/globals/<map name>
-// Other programs can interact with it through
-// the BPF_OBJ_GET bpf command.
-#define PIN_GLOBAL_NS 2
-#define PIN_OBJECT_NS 1
-
 // Nicer way to call bpf_trace_printk()
 #define printk(fmt, ...)						\
 		({							\
@@ -33,19 +25,6 @@ struct cls_entry {
     __u32 sph;
     unsigned char next_hop[ETH_ALEN];
 }__attribute__((packed));
-
-// Specific map structure used by tc and iproute2
-// Extracted from iproute2 source code:
-//      iproute2/include/bpf_elf.h
-struct bpf_elf_map {
-	__u32 type;
-	__u32 size_key;
-	__u32 size_value;
-	__u32 max_elem;
-	__u32 flags;
-	__u32 id;
-	__u32 pinning;
-};
 
 struct ip_5tuple {
     __u32 ip_src;
