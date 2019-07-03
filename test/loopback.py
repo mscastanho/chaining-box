dc3ce6b456a648a41b71cf7daed2a906decaae4b
Matheus Castanho <mscastanho@gmail.com>
2019-07-03 19:11:45 -0300

    Filter flow to avoid loopback.py messing the net

--- test/loopback.py
+++ test/loopback.py
@@ -12,7 +12,12 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+if len(sys.argv) != 3:
+    print "Usage: %s <interface> <flow src IP>" % sys.argv[0]
+    exit(1)
+
 iface = sys.argv[1]
+ipsrc = sys.argv[2]
 
 rawSocket=socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.htons(0x0800))
 rawSocket.bind((iface,0))
@@ -22,6 +27,16 @@ count=0
 
 while True:
     receivedPacket=rawSocket.recv(65536)
+
+    ipHeader=receivedPacket[14:34]
+    ipHdr=struct.unpack("!12s4s4s",ipHeader)
+    destinationIP=socket.inet_ntoa(ipHdr[2])
+    sourceIP=socket.inet_ntoa(ipHdr[1])
+
+    if sourceIP != ipsrc:
+        #print "src: " + sourceIP + " dst: " + destinationIP
+        continue
+
     try:
         rawSocket.send(receivedPacket)
     except Exception as e:
