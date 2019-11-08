6bc6e1540dfc4590cbd9806bc8243c1f85481343
Matheus Castanho <mscastanho@gmail.com>
2019-11-07 20:10:00 -0500

    Add script to show software versions on container

---
+++ build-tools/versions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "clang: $(clang --version)"
+echo "llvm: $(llvm-objdump --version)"
+echo "bpftool: $(bpftool --version)"
+echo "perf: $(perf --version)"
+echo "iproute2: $(ip -V)"
