8ca0a17b64c6ecedadc75dc6797238a0e25eafd3
Matheus Castanho <mscastanho@gmail.com>
2019-11-13 22:58:35 -0300

    [agent] Add parsing of JSON chain configuration
    Now the agent is capable of parsing a JSON config file with the
    definition of the chains to be deployed and starts+configs the
    necessary containers to make that chain deployment.
    
    A config example is also provided.
    
    At this point the agent does:
      - Config file parsing
      - Node creation (as Docker containers)
      - Creation of direct links where needed
    
    It DOES NOT do (yet):
      - Configuration of BPF stages
      - Configuration of BPF rules
      - SR-IOV VF attribution

--- src/control/agent.go
+++ src/control/agent.go
@@ -5,9 +5,11 @@ import (
   "context"
   "errors"
   "fmt"
+  "io/ioutil"
   "log"
   "os"
   "os/exec"
+  "encoding/json"
 
   /* Interaction with Docker Engine */
   "github.com/docker/docker/api/types"
@@ -26,6 +28,24 @@ const (
   SF  CBNodeType = iota   /* Service Function */
   CLS                     /* Classifier */
 )
+
+type CBInstance struct {
+  Tag string
+  Type string
+  Remote bool
+  ContainerId string
+}
+
+type CBChain struct {
+  Id int64
+  Nodes []string
+}
+
+type CBConfig struct {
+  Chains []CBChain
+  Functions []CBInstance
+  name2idx map[string]int
+}
 
 func (ntype CBNodeType) String() string {
 
@@ -53,14 +73,20 @@ var source_dir = "/home/mscastanho/devel/chaining-box"
 var target_dir = "/usr/src/chaining-box"
 
 /* Direct Link global ID */
-var dlid = 0
-
+var dlgid = 0
 
 func getDirectLinkNames() (string,string) {
-  l0 := fmt.Sprintf("dl%di0",dlid)
-  l1 := fmt.Sprintf("dl%di1",dlid)
-  dlid++
-  return l0,l1
+  /* The name of each interface on a veth pair used for direct linking two
+   * containers will follow a simple naming scheme: dl<id>[a|b].
+   *    Ex: 'dl0a' and 'dl0b'
+   *
+   * 'dl' stands * for 'direct link', <id> is a global unique id used for direct
+   * links and the last character just identifies the end corresponding end of
+   * the pair (either 'a' or 'b'). */
+  a := fmt.Sprintf("dl%da",dlgid)
+  b := fmt.Sprintf("dl%db",dlgid)
+  dlgid++
+  return a,b
 }
 
 func CreateNewContainer(name string) (string, error) {
@@ -179,25 +205,74 @@ func ConfigureStages(container string, ntype CBNodeType, iface string) error {
   return nil
 }
 
+func ParseChainsConfig(cfgfile string) (cfg CBConfig){
+  /* TODO: Loading the entire file to memory may fail if the file is too big */
+  cfgjson, err := ioutil.ReadFile(cfgfile)
+  if err != nil {
+    panic(err)
+  }
+
+  json.Unmarshal([]byte(cfgjson), &cfg)
+  // fmt.Printf("Chains: %v\nFunctions: %v\n", cfg.Chains, cfg.Functions)
+  return cfg
+}
+
 func main() {
   /* Slice containing IDs of all containers created */
   var clist []string
 
-  /* TODO: Make the number of containers to be created configurable */
-  for i := 0; i < 2; i++ {
-    cname := fmt.Sprintf("sfc%d",i)
-    id, err := CreateNewContainer(cname)
+  cfg := ParseChainsConfig("chains-config.json")
+  cfg.name2idx = make(map[string]int)
+
+  /* Start containers for all functions declared */
+  for i := 0 ; i < len(cfg.Functions) ; i++ {
+    sf := &cfg.Functions[i]
+
+    /* TODO: We should only start containers for functions that are NOT
+     * remote. This is not being done now because the current tests will
+     * all take place on the same host, but we also want to exercise the
+     * connections WITHOUT direct links (veth pairs), so we use the remote
+     * param to force connections between some containers to not use veth.*/
+    id, err := CreateNewContainer(sf.Tag)
     if err != nil {
       fmt.Println("Failed to start container!")
       panic(err)
     }
 
     clist = append(clist,id)
-    fmt.Printf("Container %s(%s) started!\n", cname, id[:6])
+    sf.ContainerId = id
+    fmt.Printf("Container %s(%s) started!\n", sf.Tag, id[:6])
+
+    /* Add name -> index mapping to internal map */
+    cfg.name2idx[sf.Tag] = i
   }
 
-  i0, i1 := CreateDirectLink(clist[0],clist[1])
+  fmt.Printf("Chains: %v\nFunctions: %v\n", cfg.Chains, cfg.Functions)
+  // fmt.Printf("name2idx: %v\n", cfg.name2idx)
 
-  ConfigureStages(clist[0],CLS,i0)
-  ConfigureStages(clist[1],SF,i1)
+  /* Now that we have all containers created, we can configure the direct
+   * links (veth pairs) where needed. */
+  for _,chain := range cfg.Chains {
+    clen := len(chain.Nodes)
+
+    /* Check bogus length */
+    if clen < 2 {
+      fmt.Printf("Chaing %d has only %d SFs. Not enough. Skipping...\n",
+          chain.Id, clen)
+      continue
+    }
+
+    /* Create direct links */
+    for i := 1 ; i < clen ; i++ {
+      /* TODO: Encapsulate this on a method for CBConfig struct */
+      n1 := &cfg.Functions[cfg.name2idx[chain.Nodes[i-1]]]
+      n2 := &cfg.Functions[cfg.name2idx[chain.Nodes[i]]]
+
+      fmt.Printf("Node1: %v\nNode2: %v\n\n", n1, n2)
+      /* We can only create veth pairs between local nodes */
+      if !n1.Remote && !n2.Remote {
+        _,_ = CreateDirectLink(n1.ContainerId,n2.ContainerId)
+      }
+    }
+  }
 }
