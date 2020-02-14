3c219f36138dfa88c981c0438868ce3a76b5a7d4
Matheus Castanho <mscastanho@gmail.com>
2020-02-13 23:16:17 -0300

    Handle cases needing artificial proxy rules

--- src/control/cbox/definitions.go
+++ src/control/cbox/definitions.go
@@ -1,8 +1,10 @@
 package cbox
 
 import (
+	"encoding/binary"
   "encoding/json"
-  // "fmt"
+	"errors"
+  "fmt"
   "net"
 )
 
@@ -51,6 +53,7 @@ type CBInstance struct {
 type CBChain struct {
   Id uint32
   Nodes []string
+  Flows []Ip_5tuplestr
 }
 
 type CBConfig struct {
@@ -81,7 +84,7 @@ func (cbc *CBConfig) parseChainsConfig(cfgbytes []byte) {
   json.Unmarshal([]byte(cfgbytes), &cbc)
   /* TODO: Validate Chain Id is < 2^24 */
   /* TODO: Validate taht chain lengths are < 256 */
-  // fmt.Printf("Chains: %v\nFunctions: %v\n", cfg.Chains, cfg.Functions)
+  fmt.Printf("Chains: %v\nFunctions: %v\n", cbc.Chains, cbc.Functions)
 }
 
 type Fwd_entry struct {
@@ -94,6 +97,75 @@ type Fwd_rule struct {
   Val Fwd_entry `json:"val"`
 }
 
+type Ip_5tuplestr struct {
+  Ipsrc string  `json:"ipsrc"`
+  Ipdst string  `json:"ipdst"`
+  Sport uint16  `json:"sport"`
+  Dport uint16  `json:"dport"`
+  Proto uint8   `json:"proto"`
+}
+
+func ip2int(ip net.IP) uint32 {
+	if len(ip) == 16 {
+		return binary.BigEndian.Uint32(ip[12:16])
+	}
+	return binary.BigEndian.Uint32(ip)
+}
+
+func int2ip(nn uint32) net.IP {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, nn)
+	return ip
+}
+
+func (tstr *Ip_5tuplestr) Parse() (*Ip_5tuple,error) {
+  ip5t := new(Ip_5tuple)
+
+  ipsrc := net.ParseIP(tstr.Ipsrc)
+  if ipsrc == nil {
+    return nil,errors.New("Failed to parse source IP")
+  }
+	ip5t.Ipsrc = ip2int(ipsrc)
+
+  ipdst := net.ParseIP(tstr.Ipdst)
+  if ipdst == nil {
+    return nil,errors.New("Failed to parse destination IP")
+  }
+	ip5t.Ipdst = ip2int(ipdst)
+
+  return ip5t,nil
+
+}
+
+type Ip_5tuple struct {
+  Ipsrc uint32 `json:"ipsrc"`
+  Ipdst uint32 `json:"ipdst"`
+  Sport uint16 `json:"sport"`
+  Dport uint16 `json:"dport"`
+  Proto uint8  `json:"proto"`
+}
+
+
+type Proxy_rule struct {
+  Key Ip_5tuple  `json:"key"`
+  Val uint32		 `json:"val"`
+}
+
+type Nsh_header struct {
+  Basic_info uint16 `json:"basic_info"`
+  Md_type    uint8  `json:"md_type"`
+  Next_proto uint8  `json:"next_proto"`
+  Serv_path  uint8  `json:"serv_path"`
+}
+
+// var default_nsh := Nsh_header {
+	// Basic_info: 0x,
+	// Md_type: 0x2,
+	// Next_proto: 0xaa,
+	// Serv_path: 0,
+// }
+
 type CBRulesConfig struct {
   Fwd []Fwd_rule `json:"fwd"`
+  Proxy []Proxy_rule `json:"proxy"`
 }
--- src/control/cbox/manager.go
+++ src/control/cbox/manager.go
@@ -60,7 +60,8 @@ func (cbm *CBManager) batchInstallRules(rcm map[string]CBRulesConfig) error {
 
 func (cbm *CBManager) InstallConfiguration(cfg *CBConfig) error {
   var sph uint32
-  var r Fwd_rule
+  var fr Fwd_rule
+  var pr Proxy_rule
   var flags uint8
   var address_next CBAddress
 
@@ -76,7 +77,8 @@ func (cbm *CBManager) InstallConfiguration(cfg *CBConfig) error {
   }
 
   /* Each node will have a list of rules */
-  rules := make(map[string][]Fwd_rule)
+  frules := make(map[string][]Fwd_rule)
+  prules := make(map[string][]Proxy_rule)
 
   for _,chain := range cfg.Chains {
     spi := chain.Id << 8
@@ -90,21 +92,51 @@ func (cbm *CBManager) InstallConfiguration(cfg *CBConfig) error {
       } else {
         flags = 0
         next := chain.Nodes[i+1]
-        /*TODO: This function should be run from the context of a CBManager, because the Address to be used
-          here is the one reported by each node on the initial Hello message */
         address_next = cbm.connectedAgents[next].addr
       }
 
-      r.Key = sph
-      r.Val = Fwd_entry{Flags: flags, Address: address_next}
-      rules[node] = append(rules[node], r)
+      fr.Key = sph
+      fr.Val = Fwd_entry{Flags: flags, Address: address_next}
+      frules[node] = append(frules[node], fr)
+
+      /* Imagine this situation: a chain with two local SFs followed by a remote one.
+       *    sf1 (local) -> sf2 (local) -> sf3 (remote)
+       * the connection sf1 -> sf2 will be configured with a direct link, and the Dec
+       * stage on sf2 will be suppressed. So the NSH table will not have any entries
+       * when the Enc stage on sf2 executes. 
+       *
+       * To avoid this problem, we need to populate the NSH data (Proxy table) on sf2
+       * with an artificial entry, so the Enc stage can use it when preparing to send
+       * the packet to sf3. */
+      if i > 0 && i < len(chain.Nodes) - 1 {
+				prev := cfg.GetNodeByName(chain.Nodes[i-1])
+				curr := cfg.GetNodeByName(chain.Nodes[i])
+				next := cfg.GetNodeByName(chain.Nodes[i+1])
+
+				if !(prev.Remote) && !(curr.Remote) && next.Remote {
+					for _,flow := range chain.Flows {
+						ip5tuple,err := flow.Parse()
+						if err != nil {
+							panic(err)
+						}
+						pr.Key = *ip5tuple
+						pr.Val = sph
+						prules[node] = append(prules[node], pr)
+					}
+				}
+        /* Create proxy rule */
+      }
     }
   }
 
   node_cfgs := make(map[string]CBRulesConfig)
 
-  for node, fr := range rules {
-    node_cfgs[node] = CBRulesConfig{Fwd: fr}
+  for node, frule := range frules {
+		if prule,ok := prules[node]; ok {
+			node_cfgs[node] = CBRulesConfig{Fwd: frule, Proxy: prule}
+		} else {
+			node_cfgs[node] = CBRulesConfig{Fwd: frule}
+		}
   }
 
   return cbm.batchInstallRules(node_cfgs)
