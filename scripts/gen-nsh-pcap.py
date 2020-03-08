1cb4437b15e545e174a75ed106c6ab7b80516f20
Matheus Castanho <mscastanho@gmail.com>
2020-03-08 10:59:53 -0300

    Update PCAP generation script
    Use MDType = 1 for NSH packets and also add new param to specify
    the directory to save files to.

--- scripts/gen-nsh-pcap.py
+++ scripts/gen-nsh-pcap.py
@@ -5,14 +5,15 @@ from scapy.contrib.nsh import NSH
 import netifaces
 import sys
 
-if len(sys.argv) != 5:
-    print('Usage: {} <i-src-mac> <i-dst-mac> <o-src-mac> <o-dst-mac>'.format(sys.argv[0]))
+if len(sys.argv) != 6:
+    print('Usage: {} <out-dir> <i-src-mac> <i-dst-mac> <o-src-mac> <o-dst-mac>'.format(sys.argv[0]))
     exit(1)
 
-i_src_mac = sys.argv[1]
-i_dst_mac = sys.argv[2]
-o_src_mac = sys.argv[3]
-o_dst_mac = sys.argv[4]
+outdir = sys.argv[1]
+i_src_mac = sys.argv[2]
+i_dst_mac = sys.argv[3]
+o_src_mac = sys.argv[4]
+o_dst_mac = sys.argv[5]
 
 # Generate input packets
 i_eth = Ether(src=i_src_mac,dst=i_dst_mac)
@@ -20,7 +21,7 @@ ip = IP(src="10.10.0.1",dst="10.10.0.2")
 udp = UDP(sport=1000,dport=2000)
 o_eth = Ether(src=o_src_mac,dst=o_dst_mac)
 
-headers = o_eth/NSH(mdtype=2, spi=100, si=255)/i_eth/ip/udp
+headers = o_eth/NSH(mdtype=1, spi=100, si=255)/i_eth/ip/udp
 
 for final_size in [64, 128, 256, 512, 1024, 1280, 1500]:
     payload = Raw(RandString(size=(final_size - len(headers))))
@@ -32,4 +33,4 @@ for final_size in [64, 128, 256, 512, 1024, 1280, 1500]:
     #  hexdump(pkt)
 
     # Save to PCAP file
-    wrpcap('/tmp/cb/pcap/nsh-traffic-{}.pcap'.format(final_size), pkt)
+    wrpcap('{}/nsh-traffic-{}.pcap'.format(outdir, final_size), pkt)
