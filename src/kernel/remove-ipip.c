1e52780914fa234a455d858fafe236e2eb55965a
Matheus Castanho <matheus.castanho@dcc.ufmg.br>
2020-10-23 17:46:02 -0300

    Make IPIP decapper also reply to ping requests

--- src/kernel/remove-ipip.c
+++ src/kernel/remove-ipip.c
@@ -4,6 +4,7 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <linux/icmp.h>
 #include <linux/pkt_cls.h>
 
 #include <stdint.h>
@@ -16,6 +17,24 @@
 #include "cb_common.h"
 #include "nsh.h"
 
+static __always_inline void set_src_mac(struct ethhdr *eth){
+  eth->h_source[0]=0xaa;
+  eth->h_source[1]=0xbb;
+  eth->h_source[2]=0xcc;
+  eth->h_source[3]=0xdd;
+  eth->h_source[4]=0xee;
+  eth->h_source[5]=0xff;
+
+  /* TODO: This is hardcoded for the value we use in the testbed. Should be
+     configured using a map like we do for the xdp/decap stage. */
+  eth->h_dest[0]=0x00;
+  eth->h_dest[1]=0x15;
+  eth->h_dest[2]=0x4d;
+  eth->h_dest[3]=0x13;
+  eth->h_dest[4]=0x81;
+  eth->h_dest[5]=0x7f;
+}
+
 SEC("xdp/decap")
 int decap_ipip(struct xdp_md *ctx)
 {
@@ -23,6 +42,7 @@ int decap_ipip(struct xdp_md *ctx)
   void *data = (void *)(long)ctx->data;
   struct ethhdr *eth;
   struct iphdr *ip;
+  struct icmphdr *icmp;
   int offset = 0;
   uint16_t h_proto = 0;
   int ret = 0;
@@ -58,12 +78,47 @@ int decap_ipip(struct xdp_md *ctx)
   if (ret)
     return XDP_DROP;
 
+  cb_debug("[RIPIP] Decapsulated packet; Size after: %d\n",data_end-data);
+
   data_end = (void *)(long)ctx->data_end;
   data = (void *)(long)ctx->data;
 
-  cb_debug("[RIPIP] Decapsulated packet; Size after: %d\n",data_end-data);
+  eth = data;
+  if (data + sizeof(struct ethhdr) > data_end)
+    return XDP_PASS;
 
-  return XDP_PASS;
+  eth = data;
+  ip = data + sizeof(struct ethhdr); // We already know there is an IP header there
+
+  if ((void*)ip + sizeof(struct iphdr) > data_end)
+    return XDP_PASS;
+
+  if (ip->protocol != IPPROTO_ICMP)
+    return XDP_PASS;
+
+  cb_debug("[RIPIP] Got ICMP packet\n");
+
+  icmp = (void*)ip + sizeof(struct iphdr);
+
+  if ((void*)icmp + sizeof(struct icmphdr) > data_end)
+    return XDP_PASS;
+
+  if (icmp->type != ICMP_ECHO)
+    return XDP_PASS;
+
+  cb_debug("[RIPIP] Got a ping request\n");
+
+  /* Prepare reply */
+  set_src_mac(eth);
+  icmp->type = ICMP_ECHOREPLY;
+  __be32 aux = ip->saddr;
+  ip->saddr = ip->daddr;
+  ip->daddr = aux;
+  icmp->checksum = 0; /* Ignore the checksum for now. */
+
+  cb_debug("[RIPIP] Sending ping reply \n");
+
+  return XDP_TX;
 }
 
 char _license[] SEC("license") = "GPL";
